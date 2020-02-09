f56976dcd695df28395a1978f35e457d98694da7
rhedgeco <rhedgeco@gmail.com>
2020-02-09 15:02:06 -0800

    Added ThreadDispatcher, ThreadWorker, and the project AssemblyDefinition

---
+++ Scripts/ThreadDispatcher.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+namespace UnityMultithreading
+{
+    [DisallowMultipleComponent]
+    public class ThreadDispatcher : MonoBehaviour
+    {
+        public static ThreadDispatcher Dispatcher { get; private set; }
+
+        private List<ThreadWorker> Workers { get; } = new List<ThreadWorker>();
+
+        private void Awake()
+        {
+            CreateSingleton();
+        }
+
+        private void Update()
+        {
+            UpdateWorkers();
+        }
+
+        private void CreateSingleton()
+        {
+            if (!Dispatcher) Dispatcher = this;
+            if (Dispatcher != this) Destroy(gameObject);
+            DontDestroyOnLoad(gameObject);
+        }
+
+        private void UpdateWorkers()
+        {
+            for (int i = Workers.Count - 1; i >= 0; i--)
+            {
+                ThreadWorker worker = Workers[i];
+                switch (worker.State)
+                {
+                    case ThreadWorker.WorkerState.Running:
+                        worker.OnUpdateWorker.Invoke();
+                        break;
+                    case ThreadWorker.WorkerState.Closed:
+                        worker.OnCloseWorker.Invoke();
+                        Workers.RemoveAt(i);
+                        break;
+                    case ThreadWorker.WorkerState.Error:
+                        worker.OnErrorWorker.Invoke();
+                        Workers.RemoveAt(i);
+                        break;
+                }
+            }
+        }
+
+        public void StartWorker(ThreadWorker worker)
+        {
+            Thread t = new Thread(worker.WorkHandleStart);
+            worker.OnStartWorker.Invoke();
+            t.Start();
+            Workers.Add(worker);
+        }
+    }
+}
---
+++ Scripts/ThreadWorker.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine.Events;
+
+namespace UnityMultithreading
+{
+    public abstract class ThreadWorker
+    {
+        public enum WorkerState
+        {
+            Running,
+            Closed,
+            Error
+        }
+
+        public WorkerState State { get; protected set; }
+        
+        protected internal UnityEvent OnStartWorker { get; } = new UnityEvent();
+        protected internal UnityEvent OnUpdateWorker { get; } = new UnityEvent();
+        protected internal UnityEvent OnCloseWorker { get; } = new UnityEvent();
+        protected internal UnityEvent OnErrorWorker { get; } = new UnityEvent();
+
+        internal void WorkHandleStart()
+        {
+            try
+            {
+                State = WorkerState.Running;
+                WorkBody();
+                State = WorkerState.Closed;
+            }
+            catch (Exception)
+            {
+                State = WorkerState.Error;
+                throw;
+            }
+        }
+
+        protected abstract void WorkBody();
+    }
+}
