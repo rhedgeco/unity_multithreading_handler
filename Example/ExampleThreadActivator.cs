c5c93b28eb9ff9f416d0390429b8750e01778eb5
rhedgeco <rhedgeco@gmail.com>
2020-02-10 08:47:24 -0800

    Added example button activator

---
+++ Example/ExampleThreadActivator.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityMultithreading;
+
+namespace UnityMultithreadingHandler.Example
+{
+    [RequireComponent(typeof(Button))]
+    public class ExampleThreadActivator : MonoBehaviour
+    {
+        private void Awake()
+        {
+            // Attach the method to the button to activate it when clicked
+            GetComponent<Button>().onClick.AddListener(ActivateExampleThread);
+        }
+
+        public void ActivateExampleThread()
+        {
+            // Create it.
+            ExampleThreadWorker worker = new ExampleThreadWorker();
+            // Start it.
+            ThreadDispatcher.StartWorker(worker);
+            // Profit.
+        }
+    }
+}
