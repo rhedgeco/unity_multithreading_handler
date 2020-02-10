3a17576185005ec3b2c08f82eef8d2455dd32378
rhedgeco <rhedgeco@gmail.com>
2020-02-10 08:30:34 -0800

    Added docstrings and additional helpers in ThreadDispatcher.

--- Scripts/ThreadDispatcher.cs
+++ Scripts/ThreadDispatcher.cs
@@ -1,38 +1,77 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using UnityEngine;
 
 namespace UnityMultithreading
 {
+    /// <summary>
+    /// ThreadDispatcher is in charge of running all ThreadWorkers
+    /// </summary>
+    /// <remarks>
+    /// It should be attached to an empty GameObject in the Unity scene for it to function.
+    /// It will force itself into a singleton.
+    /// </remarks>
     [DisallowMultipleComponent]
     public class ThreadDispatcher : MonoBehaviour
     {
-        public static ThreadDispatcher Dispatcher { get; private set; }
+        /// <value>Static instance of the singleton in the Unity Scene</value>
+        private static ThreadDispatcher _dispatcher;
 
-        private List<ThreadWorker> Workers { get; } = new List<ThreadWorker>();
+        /// <value>Dictionary containing all ThreadWorkers and associated Threads currently being handled</value>
+        private static Dictionary<ThreadWorker, Thread> Workers { get; } = new Dictionary<ThreadWorker, Thread>();
 
+        /// <value>Gets the number of ThreadWorkers being handled by ThreadDispatcher</value>
+        public static int ActiveThreadWorkers => Workers.Count;
+
+        /// <summary>
+        /// Runs on creation of the attached GameObject
+        /// </summary>
         private void Awake()
         {
             CreateSingleton();
         }
 
+        /// <summary>
+        /// Runs every frame of Unity Scene when attached to a GameObject
+        /// </summary>
         private void Update()
         {
             UpdateWorkers();
         }
 
+        /// <summary>
+        /// Forces throwing a NullReferenceException if the ThreadDispatcher has not been attached to a GameObject
+        /// </summary>
+        /// <exception cref="NullReferenceException"></exception>
+        private static void EnsureInstantiated()
+        {
+            if (!_dispatcher)
+                throw new NullReferenceException(
+                    "You must have an existing ThreadDispatcher in the scene to use it"
+                );
+        }
+
+        /// <summary>
+        /// Forces ThreadDispatcher into a Singleton state when attached to a GameObject
+        /// </summary>
         private void CreateSingleton()
         {
-            if (!Dispatcher) Dispatcher = this;
-            if (Dispatcher != this) Destroy(gameObject);
+            if (!_dispatcher) _dispatcher = this;
+            if (_dispatcher != this) Destroy(gameObject);
             DontDestroyOnLoad(gameObject);
         }
 
+        /// <summary>
+        /// Handles all ThreadWorkers based on their state
+        /// </summary>
         private void UpdateWorkers()
         {
-            for (int i = Workers.Count - 1; i >= 0; i--)
+            ThreadWorker[] keys = Workers.Keys.ToArray();
+            for (int i = keys.Length - 1; i >= 0; i--)
             {
-                ThreadWorker worker = Workers[i];
+                ThreadWorker worker = keys[i];
                 switch (worker.State)
                 {
                     case ThreadWorker.WorkerState.Running:
@@ -40,22 +79,67 @@ namespace UnityMultithreading
                         break;
                     case ThreadWorker.WorkerState.Closed:
                         worker.OnCloseWorker.Invoke();
-                        Workers.RemoveAt(i);
+                        Workers.Remove(worker);
                         break;
                     case ThreadWorker.WorkerState.Error:
                         worker.OnErrorWorker.Invoke();
-                        Workers.RemoveAt(i);
+                        Workers.Remove(worker);
                         break;
                 }
             }
         }
 
-        public void StartWorker(ThreadWorker worker)
+        /// <summary>
+        /// Starts and adds a ThreadWorker into the dispatcher's system
+        /// </summary>
+        /// <param name="worker">ThreadWorker to be started</param>
+        public static void StartWorker(ThreadWorker worker)
         {
+            EnsureInstantiated();
             Thread t = new Thread(worker.WorkHandleStart);
             worker.OnStartWorker.Invoke();
             t.Start();
-            Workers.Add(worker);
+            Workers.Add(worker, t);
+        }
+
+        /// <summary>
+        /// Checks if a ThreadWorker is currently being handled by the dispatcher
+        /// </summary>
+        /// <param name="worker">ThreadWorker to check</param>
+        /// <returns>true or false if the thread is in the dispatcher</returns>
+        public static bool ContainsThreadWorker(ThreadWorker worker)
+        {
+            EnsureInstantiated();
+            return Workers.ContainsKey(worker);
+        }
+
+        /// <summary>
+        /// Forcefully aborts a thread associated with a thread worker
+        /// </summary>
+        /// <param name="worker">ThreadWorker to force abort</param>
+        public static void ForceAbortThreadWorker(ThreadWorker worker)
+        {
+            EnsureInstantiated();
+            if (!ContainsThreadWorker(worker)) return;
+            Thread t = Workers[worker];
+            if (!t.IsAlive) return;
+            t.Abort();
+            Workers.Remove(worker);
+        }
+
+        /// <summary>
+        /// Forcefully aborts all threads being managed by the dispatcher
+        /// </summary>
+        public static void ForceAbortAllThreadWorkers()
+        {
+            EnsureInstantiated();
+            ThreadWorker[] keys = Workers.Keys.ToArray();
+            for (int i = keys.Length - 1; i >= 0; i--)
+            {
+                Thread t = Workers[keys[i]];
+                if(t.IsAlive) t.Abort();
+                Workers.Remove(keys[i]);
+            }
         }
     }
 }
--- Scripts/ThreadWorker.cs
+++ Scripts/ThreadWorker.cs
@@ -3,8 +3,14 @@ using UnityEngine.Events;
 
 namespace UnityMultithreading
 {
+    /// <summary>
+    /// Abstract class for an object to be handled by a ThreadDispatcher
+    /// </summary>
     public abstract class ThreadWorker
     {
+        /// <summary>
+        /// Enum representing the state of a ThreadWorker
+        /// </summary>
         public enum WorkerState
         {
             Running,
@@ -12,13 +18,26 @@ namespace UnityMultithreading
             Error
         }
 
+        /// <value>The current state of the ThreadWorker</value>
         public WorkerState State { get; protected set; }
         
+        /// <value>Callback event for when the ThreadWorker is started by ThreadDispatcher</value>
+        /// <remarks>Will be run on the main thread</remarks>
         protected internal UnityEvent OnStartWorker { get; } = new UnityEvent();
+        /// <value>Callback event for when the ThreadWorker is updated by ThreadDispatcher</value>
+        /// <remarks>Will be run on the main thread</remarks>
         protected internal UnityEvent OnUpdateWorker { get; } = new UnityEvent();
+        /// <value>Callback event for when the ThreadWorker found closed by ThreadDispatcher</value>
+        /// <remarks>Will be run on the main thread</remarks>
         protected internal UnityEvent OnCloseWorker { get; } = new UnityEvent();
+        /// <value>Callback event for when the ThreadWorker found in an error state by ThreadDispatcher</value>
+        /// <remarks>Will be run on the main thread</remarks>
         protected internal UnityEvent OnErrorWorker { get; } = new UnityEvent();
 
+        /// <summary>
+        /// Wraps around the WorkBody method to provide State changing and error handling
+        /// </summary>
+        /// <remarks>Will be run on a separate thread and must not do main thread work.</remarks>
         internal void WorkHandleStart()
         {
             try
@@ -34,6 +53,10 @@ namespace UnityMultithreading
             }
         }
 
+        /// <summary>
+        /// The working body of the ThreadWorker
+        /// </summary>
+        /// <remarks>Will be run on a separate thread and must not do main thread work.</remarks>
         protected abstract void WorkBody();
     }
 }
